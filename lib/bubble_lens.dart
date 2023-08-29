22f7e041b83ae681ac25662d65d3abc6f8981a7f
romancores <225000@niuitmo.ru>
2023-08-29 12:01:23 -0500

    - inertnost

--- lib/bubble_lens.dart
+++ lib/bubble_lens.dart
@@ -24,8 +24,8 @@ class BubbleLens extends StatefulWidget {
     this.itemSize = 100,
     this.paddingX = 10,
     this.paddingY = 0,
-    this.itemMaxScale = .97,
-    this.verticalStepCoefficient = 1.15,
+    this.itemMaxScale = .99,
+    this.verticalStepCoefficient = 1.3,
     this.duration = const Duration(milliseconds: 100),
     // this.radius = const Radius.circular(999),
   }) : super(key: key);
@@ -35,6 +35,7 @@ class BubbleLens extends StatefulWidget {
 }
 
 class BubbleLensState extends State<BubbleLens> {
+  static const _scaleFactor = .2;
   double _middleX = 0;
   double _middleY = 0;
   double _offsetX = 0;
@@ -138,19 +139,19 @@ class BubbleLensState extends State<BubbleLens> {
           }
         },
         onPanEnd: (details) {
-          double deltaVelocityX = details.velocity.pixelsPerSecond.dx / widget.itemSize;
-          double deltaVelocityY = details.velocity.pixelsPerSecond.dy / widget.itemSize;
+          final double deltaVelocityX = details.velocity.pixelsPerSecond.dx / widget.itemSize;
+          final double deltaVelocityY = details.velocity.pixelsPerSecond.dy / widget.itemSize;
 
             setState(() {
-              _offsetX += (_offsetX + deltaVelocityX)%step;
-              _offsetY += (_offsetY+deltaVelocityY)%step;
+              _offsetX += (_offsetX + (deltaVelocityX *details.velocity.pixelsPerSecond.distanceSquared))%step;
+              _offsetY += (_offsetY+(deltaVelocityY*details.velocity.pixelsPerSecond.distanceSquared))%step;
             });
         },
         child: Stack(
             children: widget.widgets.map((item) {
           int index = widget.widgets.indexOf(item);
-          double left;
-          double top;
+          late final double left;
+          late final double top;
           if (index == 0) {
             left = _offsetX;
             top = _offsetY;
@@ -168,23 +169,23 @@ class BubbleLensState extends State<BubbleLens> {
             top = _lastY + step[1];
           }
           _minLeft = min(
-              _minLeft, -(left - _offsetX) + _middleX - (widget.itemSize / 2));
+              _minLeft, -(left - _offsetX) + _middleX - (widget.itemSize / 2.2));
           _maxLeft =
-              max(_maxLeft, left - _offsetX + _middleX - (widget.itemSize / 2));
+              max(_maxLeft, left - _offsetX + _middleX - (widget.itemSize / 2.2));
           _minTop = min(
-              _minTop, -(top - _offsetY) + _middleY - (widget.itemSize / 2));
+              _minTop, -(top - _offsetY) + _middleY - (widget.itemSize / 2.2));
           _maxTop =
-              max(_maxTop, top - _offsetY + _middleY - (widget.itemSize / 2));
+              max(_maxTop, top - _offsetY + _middleY - (widget.itemSize / 2.2));
           _lastX = left;
           _lastY = top;
-          double distance = sqrt(
+          final double distance = sqrt(
               pow(_middleX - (left + widget.itemSize / 2), 2) +
                   pow(_middleY - (top + widget.itemSize / 2), 2));
-          double distPercent = distance / (longerSide / 2);
+          final double distPercent = distance / (longerSide / 2);
 
-          double scale = .3 * log(distPercent * -1 + 1.5) + widget.itemMaxScale;
-          scale = max(.3, min(1, (scale)));
-          if (scale.toString() == double.nan.toString()) scale = .3;
+          double scale = _scaleFactor * log(distPercent * -1 +0.8) + widget.itemMaxScale;
+          scale = max(_scaleFactor, min(1, (scale)));
+          if (scale.toString() == double.nan.toString()) scale = _scaleFactor;
 
           // double originX = min(
           //     widget.itemSize * 1,
@@ -193,14 +194,16 @@ class BubbleLensState extends State<BubbleLens> {
           //     widget.itemSize * 1,
           //     max(widget.itemSize * -1, top - _middleY)) / -2;
 
-          double fadePercent = distPercent * .9;
-          double fadeValue = .9;
+          final double fadePercent = distPercent * .7;
+          final double fadeValue = .7;
           return AnimatedPositioned(
               duration: widget.duration,
+              curve: Curves.linearToEaseOut,
               top: top,
               left: left,
               child: AnimatedScale(
                   scale: scale,
+                  curve: Curves.decelerate,
                   filterQuality: FilterQuality.medium,
                   duration: widget.duration,
                   // origin: Offset(originX, originY),
